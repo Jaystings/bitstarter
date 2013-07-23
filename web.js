7fa84c3c747ffc0675265a11d6217893e7634991
James Hastings <Hastings.James@hotmail.com>
2013-07-23 02:41:42 +0000

    New index.html, mobile compatibility

--- web.js
+++ web.js
@@ -2,14 +2,16 @@ var express = require('express');
 
 var fs = require('fs');
 
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var k = fs.readFileSync('index.html', 'utf8');
+var k = fs.readFileSync(htmlfile).toString();
 response.send( k );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on " + port)
 });
